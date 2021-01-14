e7a01bd726c8395b5ad23f242da3d3c13bc2b749
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-01-14 15:01:16 -0300

    Lauching DatePickerDialog on btn click

--- app/src/main/java/com/danieljrodrigues/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/danieljrodrigues/ageinminutes/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.danieljrodrigues.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener { view ->
+            clickDatePicker(view)
+            Toast.makeText(this, "Works", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun clickDatePicker(view: View) {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+                selectedDate.text = "$dayOfMonth/$month/$year"
+            }, year, month, day).show()
     }
 }
